b984e275a125a9c4d9abd2f6afcfda6e673775a1
Rajan <rajant02022000@gmail.com>
2026-04-05 17:28:23 +0530

    uplode

---
+++ SRC/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+app.use("/test",(req,res)=> {
+    res.send('Hello from server!');
+})
+
+
+app.use("/wellcome",(req,res)=> {
+    res.send('Wellcome from server!');
+});
+
+
+
+
+app.listen(3000,()=>{console.log('Server  is running on port 3000...')});
